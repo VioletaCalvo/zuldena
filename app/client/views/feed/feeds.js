6b871d7ea156a0504382a1b345cbd4fabe789178
Violeta Calvo <violeta@brozerly.com>
2015-10-11 11:21:30 +0200

    add instagram elements html

--- app/client/views/feed/feeds.js
+++ app/client/views/feed/feeds.js
@@ -2,5 +2,9 @@ Template.feeds.helpers({
 	twitterFeed: function () {
 		 var tweets = ApisInformations.find({service:'twitter'}, {sort: {createdAt:-1}, limit:5});
 		 return tweets;
+	},
+	instagram: function() {
+		var instagrams = ApisInformations.find({service:'instagram'}, {sort:{createdAt:-1}, limit:5});
+		return instagrams;
 	}
-})
+});
