f6b0290243fc09c368ce243260571b26aedd50f6
Violeta Calvo <violeta@brozerly.com>
2015-10-11 13:43:14 +0200

    fix connect buttons

--- app/client/views/profile/setting.js
+++ app/client/views/profile/setting.js
@@ -1,19 +1,24 @@
 Template.settings.events({
 	'click .btn-facebook': function (evt, tmpl) {
-		if (!_.contains(Accounts.oauth.serviceNames(), 'facebook')) {
-			Meteor.linkWithFacebook({}, function (error) {
-				if (error) {
-					toastr.error(error.reason);
-				}
-				else {
-					FlowRouter.go('feeds');
-					toastr.success("Facebook account connected!");
-				}
-			});
-		}
-		else{
-			toastr.success('already configured');
-		}
+		Meteor.call('checkUserServices', 'facebook', function(error, configured) {
+			if (error) {
+				toastr.error(error.reason);
+			}
+			else if (configured) {
+				toastr.success('already configured');
+			}
+			else {
+				Meteor.linkWithFacebook({}, function (error) {
+					if (error) {
+						toastr.error(error.reason);
+					}
+					else {
+						FlowRouter.go('feeds');
+						toastr.success("Facebook account connected!");
+					}
+				});
+			}
+		});
 	},
 
 	'click .btn-twitter': function (evt, tmpl) {
@@ -31,40 +36,46 @@ Template.settings.events({
 		}
 		else{
 			toastr.success('already configured');
-		}
-	},
-
-	'click .btn-youtube': function (evt, tmpl) {
-		if (!_.contains(Accounts.oauth.serviceNames(), 'google')) {
-			Meteor.linkWithGoogle({}, function (error) {
-				if (error) {
-					toastr.error(error.reason);
-				}
-				else {
-					FlowRouter.go('feeds');
-					toastr.success("Google account connected!");
-				}
-			});
-		}
-		else{
-			toastr.success('already configured');
-		}
+		}Meteor.call('checkUserServices', 'twitter', function(error, configured) {
+			if (error) {
+				toastr.error(error.reason);
+			}
+			else if (configured) {
+				toastr.success('already configured');
+			}
+			else {
+				Meteor.linkWithTwitter({}, function (error) {
+					if (error) {
+						toastr.error(error.reason);
+					}
+					else {
+						FlowRouter.go('feeds');
+						toastr.success("Twitter account connected!");
+					}
+				});
+			}
+		});
 	},
 
 	'click .btn-instagram': function (evt, tmpl) {
-		if (!_.contains(Accounts.oauth.serviceNames(), 'instagram')) {
-			Meteor.linkWithInstagram({}, function (error) {
-				if (error) {
-					toastr.error(error.reason);
-				}
-				else {
-					FlowRouter.go('feeds');
-					toastr.success("Instagram account connected!");
-				}
-			});
-		}
-		else{
-			toastr.success('already configured');
-		}
+		Meteor.call('checkUserServices', 'instagram', function(error, configured) {
+			if (error) {
+				toastr.error(error.reason);
+			}
+			else if (configured) {
+				toastr.success('already configured');
+			}
+			else {
+				Meteor.linkWithInstagram({}, function (error) {
+					if (error) {
+						toastr.error(error.reason);
+					}
+					else {
+						FlowRouter.go('feeds');
+						toastr.success("Instagram account connected!");
+					}
+				});
+			}
+		});
 	}
 });
